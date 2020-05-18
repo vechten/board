554ec459ac7c68e2eac2579a6f8f12c67466b3a3
vechten <hk190696@gmail.com>
2020-05-18 08:53:54 +0200

    Initial commit

---
+++ src/test/java/korzeniewski/hubert/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package korzeniewski.hubert.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
