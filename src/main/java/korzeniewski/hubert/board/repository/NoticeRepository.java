bd8d35c9443fd6c35d5ccfeb22fa6f8ea25703f0
vechten <hk190696@gmail.com>
2020-05-18 09:01:45 +0200

    Repository of model class Notice has been added

---
+++ src/main/java/korzeniewski/hubert/board/repository/NoticeRepository.java
@@ -0,0 +1,15 @@
+package korzeniewski.hubert.board.repository;
+
+import korzeniewski.hubert.board.model.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+
+@Repository
+@Transactional
+public interface NoticeRepository extends JpaRepository<Notice, Integer>, QueryByExampleExecutor<Notice> {
+}
+
