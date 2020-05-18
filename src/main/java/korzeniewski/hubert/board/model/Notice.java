25527e8b813f7b25afc76a5ca6674ea1f546c5af
vechten <hk190696@gmail.com>
2020-05-18 08:59:00 +0200

    Model class refactored to entity class

--- src/main/java/korzeniewski/hubert/board/model/Notice.java
+++ src/main/java/korzeniewski/hubert/board/model/Notice.java
@@ -1,4 +1,49 @@
 package korzeniewski.hubert.board.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Notice {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String author;
+    private String date;
+    private String content;
+
+    public Notice() {
+    }
+
+    public Notice(String author, String date, String content) {
+        this.author = author;
+        this.date = date;
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
